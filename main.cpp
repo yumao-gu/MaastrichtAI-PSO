74bb22eed4636d412f911941fb9a90f230647417
yumao-gu <yumao.gu@student.maastrichtuniversity.nl>
2020-02-11 17:11:53 +0800

    analyse brief

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ namespace plt = matplotlibcpp;
 #define NVARS 2                           //参数个数
 #define WMAX 0.9                           //惯量权重最大值
 #define WMIN 0.4                           //惯量权重最小值
-#define FUNC_TYPE 1                        //1代表Rosenbrock，２代表Rastrigin
+#define FUNC_TYPE 2                        //1代表Rosenbrock，２代表Rastrigin
 #define W_CHANGE_METHOD 1                  //1等比例递减
 
 struct particle{                          //单个粒子
@@ -49,37 +49,37 @@ double particle_distance(particle,double[]);
 double val_difference(particle,double);
 bool adjacent_id(particle,int);
 
-/*  简单的数值映射到(0,1) */
+/*  简单的数值映射到(0,1) writen by Gu*/
 double simple_val_map(double val)
 {
     return val/(sqrt(1 + pow(val,2)));
 }
 
-/*  计算当前粒子距离全局最优粒子距离　*/
+/*  计算当前粒子距离全局最优粒子距离　writen by Gu*/
 double particle_distance(particle p,double gbest[])
 {
     return sqrt(pow(p.x[0] - gbest[0],2) + pow(p.x[1] - gbest[1],2));
 }
 
-/*  计算当前粒子和全局最优值的差　*/
+/*  计算当前粒子和全局最优值的差　writen by Su*/
 double val_difference(particle p ,double gBest_val)
 {
     return abs(evalfunc(p.x,FUNC_TYPE) - gBest_val);
 }
 
-/*  判断是否关系临近全局最优　*/
+/*  判断是否关系临近全局最优　writen by Su*/
 bool adjacent_id(particle p,int gBest_id)
 {
     return (abs(p.id - gBest_id)  < 3) || (abs(p.id + POPSIZE - gBest_id)  < 3) || (abs(gBest_id + POPSIZE - p.id)  < 3);
 }
 
-/*  符号函数　　*/
+/*  符号函数　　writen by Gu*/
 int sign(bool flag)
 {
     return flag?1:-1;
 }
 
-/*　圆上随机产生一个向量作为随机游走　*/
+/*　圆上随机产生一个向量作为随机游走　writen by Gu*/
 vector<double> ellipse_random(particle p)
 {
     Eigen::Rotation2Dd gBest_trans(atan(gBest[1]/gBest[0]));
@@ -101,6 +101,7 @@ vector<double> ellipse_random(particle p)
     return v_ellipse;
 }
 
+/* writen by Gu */
 double avg(double parameter[], int n)
 {
     double num = 0;
@@ -111,6 +112,7 @@ double avg(double parameter[], int n)
     return num / n;
 }
 
+/* writen by Gu */
 double stddev(double parameter[], int n)
 {
     double num = avg(parameter, n);
@@ -122,13 +124,14 @@ double stddev(double parameter[], int n)
     return sqrt(sum / n);
 }
 
+/* writen by Gu */
 double randval(double low, double high)
 {
     return (low + (high - low)*rand()*1.0 / RAND_MAX);
 }
 
 /*          评估函数            */
-/* 通过参数FUNC来选择想要的评估函数 */
+/* 通过参数FUNC来选择想要的评估函数 writen by Su */
 double evalfunc(double parameter[], int FUNC = 1)
 {
     if (FUNC == 1)
@@ -141,21 +144,28 @@ double evalfunc(double parameter[], int FUNC = 1)
     if (FUNC == 2)
     {
         double val = 0;
-        for (int i = 0; i < NVARS; i++) {
-            val += (parameter[i] * parameter[i] - 10 * cos(2 * PI*parameter[i] / 180 * PI) + 10.0);
+        for (int i = 0; i < NVARS; i++)
+        {
+            val += (parameter[i] * parameter[i] - 10 * cos(2 * PI * parameter[i] ) + 10.0);
         }
         return val;
     }
 }
 
 /*          评估函数的导数            */
-/* 通过参数FUNC来选择想要的评估函数的导数 */
+/* 通过参数FUNC来选择想要的评估函数的导数 writen by Gu*/
 double evalfunc_derivative_x(double parameter[], int FUNC = 1)
 {
     if (FUNC == 1)
     {
         double val = 0;
         val = 2 * parameter[0] * (1 - 2 * (parameter[1]  - pow(parameter[0],2)));
+        return val;
+    }
+    else if(FUNC_TYPE  == 2)
+    {
+        double val = 0;
+        val = 2 * parameter[0] + 20 * PI * sin(2 * PI * parameter[0]);
         return val;
     }
     else
@@ -165,13 +175,19 @@ double evalfunc_derivative_x(double parameter[], int FUNC = 1)
 }
 
 /*          评估函数的导数            */
-/* 通过参数FUNC来选择想要的评估函数的导数 */
+/* 通过参数FUNC来选择想要的评估函数的导数 writen by Gu*/
 double evalfunc_derivative_y(double parameter[], int FUNC = 1)
 {
     if (FUNC == 1)
     {
         double val = 0;
         val = 2 * (parameter[1]  - pow(parameter[0],2));
+        return val;
+    }
+    else if(FUNC_TYPE  == 2)
+    {
+        double val = 0;
+        val = 2 * parameter[1] + 20 * PI * sin(2 * PI * parameter[1]);
         return val;
     }
     else
@@ -183,7 +199,7 @@ double evalfunc_derivative_y(double parameter[], int FUNC = 1)
 /*  初始化每个粒子的速度、位置并将  */
 /*  该位置设定为当前历史最优位置找  */
 /*  到所有粒子的最优位置并设定为当  */
-/*  前群体历史最优位置              */
+/*  前群体历史最优位置            writen by Su  */
 void initialize()
 {
     int i, j;
@@ -227,7 +243,7 @@ void initialize()
 
 /*  通过传入参数FUNC来调用不同的评  */
 /*  估函数并对粒子位置进行评估并更  */
-/*  新粒子历史最优位置              */
+/*  新粒子历史最优位置            writen by Su  */
 void evaluate()
 {
     int i, j;
@@ -250,7 +266,7 @@ void evaluate()
 
 /*  通过参数w_change_method来选择不 */
 /*  同的惯性权重衡量规则来根据群体  */
-/*  历史最优位置更新粒子速度以及位置*/
+/*  历史最优位置更新粒子速度以及位置 writen by Gu*/
 void update(int interation, int w_change_method = 1)
 {
     int i, j;
@@ -271,7 +287,7 @@ void update(int interation, int w_change_method = 1)
         double v_ellipse[NVARS];
         v_ellipse[0] = 0.05 * sqrt(pow(particles[j].v[0],2) + pow(particles[j].v[1],2)) * ellipse_random(particles[j])[0];
         v_ellipse[1] = 0.05 * sqrt(pow(particles[j].v[0],2) + pow(particles[j].v[1],2)) * ellipse_random(particles[j])[1];
-        cout << j << '\t' << sqrt(pow(particles[j].v[0],2) + pow(particles[j].v[1],2)) <<  '\t' << v_ellipse[0] << '\t' << v_ellipse[1] << endl;
+        //cout << j << '\t' << sqrt(pow(particles[j].v[0],2) + pow(particles[j].v[1],2)) <<  '\t' << v_ellipse[0] << '\t' << v_ellipse[1] << endl;
 
         double v_derivative[NVARS];
         v_derivative[0]  = 0.05 * evalfunc_derivative_x(particles[j].x);
@@ -280,11 +296,11 @@ void update(int interation, int w_change_method = 1)
         for (i = 0; i < NVARS; i++)
         {
 //            v = w*particles[j].v[i] + c1*randval(0, 1)*(particles[j].pBest[i] - particles[j].x[i])
-//                    + c2*randval(0, 1)*(gBest[i] - particles[j].x[i]) + v_ellipse[i] + v_derivative[i];
+//                    + c2*randval(0, 1)*(gBest[i] - particles[j].x[i])  + v_derivative[i];//+ v_ellipse[i];// + v_derivative[i];
             v = w*particles[j].v[i] + c1*randval(0, 1)*(particles[j].pBest[i] - particles[j].x[i])
                 + sign(interation < MAXINTERATION/3) * sign(adjacent_id(particles[j],gBest_id)) * c2 * randval(0, 1)
-                * pow((gBest[i] - particles[j].x[i]),sign(adjacent_id(particles[j],gBest_id)) * sign(interation < MAXINTERATION/3))
-                + v_ellipse[i] + v_derivative[i];
+                * pow((gBest[i] - particles[j].x[i]),sign(adjacent_id(particles[j],gBest_id)) * sign(interation < MAXINTERATION/3));
+                //+ v_ellipse[i] + v_derivative[i];
 
             if (v > vmax)
             {
@@ -316,7 +332,7 @@ void update(int interation, int w_change_method = 1)
     }
 }
 
-/*  更新群体最优位置         */
+/*  更新群体最优位置      writen by Su   */
 void fit(void)
 {
     int i, j;
@@ -337,6 +353,7 @@ void fit(void)
     }
 }
 
+/* Visialbe writen by Gu */
 void show()
 {
     std::vector<std::vector<double>> x_map, y_map, z_map;
@@ -345,8 +362,14 @@ void show()
         for (double j = -2; j <= 2; j += 0.1) {
             x_row.push_back(i);
             y_row.push_back(j);
-            z_row.push_back(pow(i,2) + 1 * pow((j - pow(i,2)),2));
-            //z_row.push_back((i * i - 10 * cos(2 * PI*i / 180 * PI) + 10.0) + (j * j - 10 * cos(2 * PI*j / 180 * PI) + 10.0));
+            if(FUNC_TYPE  == 1)
+            {
+                z_row.push_back(pow(i,2) + 1 * pow((j - pow(i,2)),2));
+            }
+            if(FUNC_TYPE  == 2)
+            {
+                z_row.push_back((i * i - 10 * cos(2 * PI*i) + 10.0) + (j * j - 10 * cos(2 * PI*j ) + 10.0));
+            }
         }
         x_map.push_back(x_row);
         y_map.push_back(y_row);
@@ -365,25 +388,64 @@ void show()
     plt::show();
 }
 
+/* writen by Gu */
 int main()
 {
-    srand(time(NULL));
-    absbound = 2.0;
-    vmax = absbound*0.15;
-
-    initialize();
-    evaluate();
-    for (int i = 0; i < MAXINTERATION; i++)
+    int done[100000];
+    int done_iter_times[100000];
+    for (int k = 1; k < 100000; k++ )
     {
-        update(i,W_CHANGE_METHOD);
+        srand(time(NULL));
+        absbound = 2;
+        vmax = absbound*0.15;
+
+        initialize();
         evaluate();
-        fit();
-        if(i % 10  == 0)
+
+
+        int iteration = MAXINTERATION;
+        for (int i = 0; i < MAXINTERATION; i++)
         {
-            show();
+            update(i, W_CHANGE_METHOD);
+            evaluate();
+            fit();
+            if (i % 10 == 0)
+            {
+                show();
+            }
+            int particle_ok = 0;
+
+            for (int j = 0; j < POPSIZE; j++)
+            {
+                if(sqrt(pow(particles[j].x[0],2) + pow(particles[j].x[1],2)) < 0.01)
+                {
+                    particle_ok++ ;
+                }
+            }
+            //std::cout << "iteration  : " << i << "    particle_ok : " << particle_ok << std::endl;
+
+            if(particle_ok > 13)
+            {
+                iteration = i;
+                break;
+            }
         }
+
+        std::cout << "iteration  : " << k << std::endl;
+        done_iter_times[k] = iteration;
+        done[k] = (iteration == MAXINTERATION)?0:1;
     }
 
+    double ave_times = 0.0;
+    int not_done_times = 0;
+    for(int i = 0; i < 100000; i++)
+    {
+        ave_times += done_iter_times[i];
+        not_done_times += 1 - done[i];
+    }
+    ave_times /= 100000.0;
+
+    std::cout << "ave_times : " << ave_times <<"\t not_done_times : " << not_done_times << std::endl;
     std::cout << "End PSO" << std::endl;
 
     return 0;
